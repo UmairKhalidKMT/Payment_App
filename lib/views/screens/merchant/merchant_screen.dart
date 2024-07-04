e3b0eb1c13c9170c3730443f4dfbc36585f9725a
Umair Khalid <umairkhalidkmt@gmail.com>
2024-07-04 14:35:01 +0500

    Used state management

--- lib/views/screens/merchant/merchant_screen.dart
+++ lib/views/screens/merchant/merchant_screen.dart
@@ -1,24 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:payment_app/controllers/merchant_form_controller.dart';
+import 'package:payment_app/models/merchant/merchant_form_model.dart';
+import 'package:provider/provider.dart';
 import 'package:payment_app/utils/app_colors.dart';
 import 'package:payment_app/views/screens/widgets/button.dart';
 
-class MerchantScreen extends StatefulWidget {
+class MerchantScreen extends StatelessWidget {
   const MerchantScreen({super.key});
 
   @override
-  State<MerchantScreen> createState() => _MerchantScreenState();
-}
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => MerchantController(),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          toolbarHeight: 60.2,
+          toolbarOpacity: 0.8,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(50),
+              bottomLeft: Radius.circular(50),
+            ),
+          ),
+          elevation: 1.0,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Text(
+            'Merchants',
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(fontSize: 22),
+            ),
+          ),
+          actions: [
+            Builder(
+              builder: (context) {
+                double screenWidth = MediaQuery.of(context).size.width;
+                return Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: IconButton(
+                    icon: const Icon(Icons.person_add_rounded),
+                    iconSize: screenWidth / 13,
+                    onPressed: () => _showAddMerchantDialog(context),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        body: Consumer<MerchantController>(
+          builder: (context, controller, child) {
+            return ListView.builder(
+              itemCount: controller.merchants.length,
+              itemBuilder: (context, index) {
+                final merchant = controller.merchants[index];
+                final isInactive = merchant.status == 'Inactive';
+                return Card(
+                  elevation: 5.0,
+                  margin: const EdgeInsets.only(
+                      left: 15.0, right: 15.0, top: 5.0, bottom: 5.0),
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  child: ListTile(
+                    tileColor: AppColors.lightBlackColor,
+                    selectedColor: AppColors.lightWhiteColor,
+                    title: Text(
+                      merchant.name,
+                      style: TextStyle(
+                        color: isInactive
+                            ? AppColors.redColor
+                            : AppColors.whiteColor,
+                      ),
+                    ),
+                    subtitle: Text(merchant.businessName),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            controller.deleteMerchant(index);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.update_rounded),
+                          onPressed: () {
+                            _showUpdateMerchantDialog(context, index, merchant);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.visibility),
+                          onPressed: () {
+                            _showMerchantDetails(context, merchant);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
 
-class _MerchantScreenState extends State<MerchantScreen> {
-  List<Map<String, String>> merchants = [];
-
-  void _addNewUser() {
-    TextEditingController nameController = TextEditingController();
-    TextEditingController phoneController = TextEditingController();
-    TextEditingController emailController = TextEditingController();
-    TextEditingController addressController = TextEditingController();
-    TextEditingController businessNameController = TextEditingController();
+  void _showAddMerchantDialog(BuildContext context) {
+    final controller = Provider.of<MerchantController>(context, listen: false);
+    final nameController = TextEditingController();
+    final phoneController = TextEditingController();
+    final emailController = TextEditingController();
+    final addressController = TextEditingController();
+    final businessNameController = TextEditingController();
     String status = 'Active';
 
     showDialog(
@@ -72,9 +170,7 @@ class _MerchantScreenState extends State<MerchantScreen> {
                         );
                       }).toList(),
                       onChanged: (newValue) {
-                        setState(() {
-                          status = newValue!;
-                        });
+                        status = newValue!;
                       },
                     ),
                   ),
@@ -92,16 +188,14 @@ class _MerchantScreenState extends State<MerchantScreen> {
             ButtonWidget(
               btnName: 'Add',
               voidCallback: () {
-                setState(() {
-                  merchants.add({
-                    'name': nameController.text,
-                    'phone': phoneController.text,
-                    'email': emailController.text,
-                    'address': addressController.text,
-                    'businessName': businessNameController.text,
-                    'status': status,
-                  });
-                });
+                controller.addMerchant(Merchant(
+                  name: nameController.text,
+                  phone: phoneController.text,
+                  email: emailController.text,
+                  address: addressController.text,
+                  businessName: businessNameController.text,
+                  status: status,
+                ));
                 Navigator.of(context).pop();
               },
               icon: const Icon(Icons.add_task_outlined),
@@ -112,18 +206,16 @@ class _MerchantScreenState extends State<MerchantScreen> {
     );
   }
 
-  void _updateUser(int index) {
-    TextEditingController nameController =
-        TextEditingController(text: merchants[index]['name']);
-    TextEditingController phoneController =
-        TextEditingController(text: merchants[index]['phone']);
-    TextEditingController emailController =
-        TextEditingController(text: merchants[index]['email']);
-    TextEditingController addressController =
-        TextEditingController(text: merchants[index]['address']);
-    TextEditingController businessNameController =
-        TextEditingController(text: merchants[index]['businessName']);
-    String status = merchants[index]['status'] ?? 'Active';
+  void _showUpdateMerchantDialog(
+      BuildContext context, int index, Merchant merchant) {
+    final controller = Provider.of<MerchantController>(context, listen: false);
+    final nameController = TextEditingController(text: merchant.name);
+    final phoneController = TextEditingController(text: merchant.phone);
+    final emailController = TextEditingController(text: merchant.email);
+    final addressController = TextEditingController(text: merchant.address);
+    final businessNameController =
+        TextEditingController(text: merchant.businessName);
+    String status = merchant.status;
 
     showDialog(
       context: context,
@@ -176,9 +268,7 @@ class _MerchantScreenState extends State<MerchantScreen> {
                         );
                       }).toList(),
                       onChanged: (newValue) {
-                        setState(() {
-                          status = newValue!;
-                        });
+                        status = newValue!;
                       },
                     ),
                   ),
@@ -196,16 +286,16 @@ class _MerchantScreenState extends State<MerchantScreen> {
             ButtonWidget(
               btnName: 'Update',
               voidCallback: () {
-                setState(() {
-                  merchants[index] = {
-                    'name': nameController.text,
-                    'phone': phoneController.text,
-                    'email': emailController.text,
-                    'address': addressController.text,
-                    'businessName': businessNameController.text,
-                    'status': status,
-                  };
-                });
+                controller.updateMerchant(
+                    index,
+                    Merchant(
+                      name: nameController.text,
+                      phone: phoneController.text,
+                      email: emailController.text,
+                      address: addressController.text,
+                      businessName: businessNameController.text,
+                      status: status,
+                    ));
                 Navigator.of(context).pop();
               },
               icon: const Icon(Icons.update),
@@ -216,7 +306,7 @@ class _MerchantScreenState extends State<MerchantScreen> {
     );
   }
 
-  void _viewUserDetails(Map<String, String> user) {
+  void _showMerchantDetails(BuildContext context, Merchant merchant) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -226,17 +316,17 @@ class _MerchantScreenState extends State<MerchantScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Name: ${user['name']}'),
+                Text('Name: ${merchant.name}'),
                 const SizedBox(height: 8),
-                Text('Phone: ${user['phone']}'),
+                Text('Phone: ${merchant.phone}'),
                 const SizedBox(height: 8),
-                Text('Email: ${user['email']}'),
+                Text('Email: ${merchant.email}'),
                 const SizedBox(height: 8),
-                Text('Address: ${user['address']}'),
+                Text('Address: ${merchant.address}'),
                 const SizedBox(height: 8),
-                Text('Business Name: ${user['businessName']}'),
+                Text('Business Name: ${merchant.businessName}'),
                 const SizedBox(height: 8),
-                Text('Status: ${user['status']}'),
+                Text('Status: ${merchant.status}'),
               ],
             ),
           ),
@@ -250,99 +340,6 @@ class _MerchantScreenState extends State<MerchantScreen> {
           ],
         );
       },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        toolbarHeight: 60.2,
-        toolbarOpacity: 0.8,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(50),
-              bottomLeft: Radius.circular(50)),
-        ),
-        elevation: 1.0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        title: Text(
-          'Merchants',
-          style: GoogleFonts.poppins(
-            textStyle: const TextStyle(fontSize: 22),
-          ),
-        ),
-        actions: [
-          Builder(
-            builder: (context) {
-              double screenWidth = MediaQuery.of(context).size.width;
-              return Padding(
-                padding: const EdgeInsets.only(right: 10.0),
-                child: IconButton(
-                  icon: const Icon(Icons.person_add_rounded),
-                  iconSize: screenWidth / 13,
-                  onPressed: _addNewUser,
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: merchants.length,
-        itemBuilder: (context, index) {
-          final merchant = merchants[index];
-          final isInactive = merchant['status'] == 'Inactive';
-          return Card(
-            elevation: 5.0,
-            margin: const EdgeInsets.only(
-                left: 15.0, right: 15.0, top: 5.0, bottom: 5.0),
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            child: ListTile(
-              tileColor: AppColors.lightBlackColor,
-              selectedColor: AppColors.lightWhiteColor,
-              title: Text(
-                merchant['name'] ?? '',
-                style: TextStyle(
-                  color: isInactive ? AppColors.redColor : AppColors.whiteColor,
-                ),
-              ),
-              subtitle: Text(merchant['businessName'] ?? ''),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      setState(() {
-                        merchants.removeAt(index);
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.update_rounded),
-                    onPressed: () {
-                      _updateUser(index);
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.visibility),
-                    onPressed: () {
-                      _viewUserDetails(merchant);
-                    },
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
